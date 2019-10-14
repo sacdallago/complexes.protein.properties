3eb717414bac10930244dc2bf4d17140188af7b8
Christian Dallago <code@dallago.us>
2019-10-14 13:14:01 +0200

    updates

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,12 @@
 import CORUM from 'data/allComplexes';
 
 const uniprot_accessions = CORUM
-    .map(e => e['subunits(UniProt IDs)'].split(';'))
-    .reduce();
+    .map(e => e['subunits(UniProt IDs)'].split(';').map(accession => {return {accession: accession, id: e['ComplexID']}}))
+    .reduce((current, previous) => previous.concat(current), []);
+
+const gene_names = CORUM
+    .map(e => e['subunits(Gene name)'].split(';').map(gene => {return {gene: gene, id: e['ComplexID']}}))
+    .reduce((current, previous) => previous.concat(current), []);
 
 const uniprotRegex = /^[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}$/;
 
