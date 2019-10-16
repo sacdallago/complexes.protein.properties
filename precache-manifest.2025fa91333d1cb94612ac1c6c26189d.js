951dbf67bd1fe986306f9c81c2e19035ec0c0108
Christian Dallago <code@dallago.us>
2019-10-16 15:04:33 +0200

    Updates

---
+++ precache-manifest.2025fa91333d1cb94612ac1c6c26189d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c58d7dea74e72806521989219cf82d89",
+    "url": "search_complexes.protein.properties/index.html"
+  },
+  {
+    "revision": "dbfb2fc18fd953cc3245",
+    "url": "search_complexes.protein.properties/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2a03921f768b464c1fba",
+    "url": "search_complexes.protein.properties/static/js/2.99f7e10c.chunk.js"
+  },
+  {
+    "revision": "dbfb2fc18fd953cc3245",
+    "url": "search_complexes.protein.properties/static/js/main.5b9452e9.chunk.js"
+  },
+  {
+    "revision": "fda0e1d28cfa259fd7b9",
+    "url": "search_complexes.protein.properties/static/js/runtime-main.5aec277e.js"
+  }
+]);
